957f09472a7b513cc41f5b595c8a5d42e4d92eea
faqulord <stylefaqu@gmail.com>
2026-01-22 13:46:40 +0100

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,4 +1,5 @@
-import { NextResponse } from 'next/server';
+import prisma from '../../../lib/prisma';
+
 import bcrypt from 'bcryptjs';
 import prisma from '../../../lib/prisma'; // EZT JAVÍTOTTUK!
 
