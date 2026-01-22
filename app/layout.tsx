97ea06e0b81d2fd9f886cdeaca9effddcc756b10
faqulord <stylefaqu@gmail.com>
2026-01-22 05:28:09 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.css"; // EZ A FONTOS SOR!
 
 const inter = Inter({ subsets: ["latin"] });
 
