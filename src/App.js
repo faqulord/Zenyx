132677dc889de06dd934cd4b4cbc858e7dd8974b
faqulord <stylefaqu@gmail.com>
2026-01-27 22:42:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,31 +2,26 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import ContentSection from './components/ContentSection';
-import ProductSection from './components/ProductSection'; // Ez maradhat "Kiemelt" résznek a főoldalon
 import ShopPage from './components/ShopPage';
 import Footer from './components/Footer';
 
 function App() {
-  // Ez figyeli, melyik oldalon vagyunk éppen (alapból: home)
   const [currentPage, setCurrentPage] = useState('home');
 
   return (
     <div className="App">
-      {/* A Navigációt mindig látjuk */}
       <Navbar setPage={setCurrentPage} currentPage={currentPage} />
 
-      {/* HA a Kezdőlapon vagyunk: */}
       {currentPage === 'home' && (
         <>
           <HeroSection />
+          {/* Főoldalon: Csak a tartalom, bemutatkozás, idézet */}
           <ContentSection />
-          {/* A főoldalon is hagyhatunk egy kis ízelítőt */}
-          <ProductSection /> 
         </>
       )}
 
-      {/* HA a Webshopon vagyunk: */}
       {currentPage === 'shop' && (
+        /* Webshop oldalon: Csak a termékek */
         <ShopPage />
       )}
 
