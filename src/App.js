44edd9920e91501e9bbbd6829ee1e8f789dc9473
faqulord <stylefaqu@gmail.com>
2026-01-27 22:04:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import HeroSection from './components/HeroSection';
+import ContentSection from './components/ContentSection'; // Ezt adtuk hozzá
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
+      <ContentSection /> {/* Itt jelenítjük meg az új részt */}
     </div>
   );
 }
