67ea96545a801e749aabc7c5657a2873ae94b0c5
faqulord <stylefaqu@gmail.com>
2026-01-27 22:16:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
 import HeroSection from './components/HeroSection';
-import ContentSection from './components/ContentSection'; // Ezt adtuk hozzá
+import ContentSection from './components/ContentSection';
+import ProductSection from './components/ProductSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      {/* 1. A kozmikus főoldal */}
       <HeroSection />
-      <ContentSection /> {/* Itt jelenítjük meg az új részt */}
+      
+      {/* 2. Az idézet, bemutatkozás és social média gombok */}
+      <ContentSection />
+      
+      {/* 3. A termékek (Könyvek, eszközök) */}
+      <ProductSection />
+      
+      {/* 4. A lábléc (Copyright, linkek) */}
+      <Footer />
     </div>
   );
 }
