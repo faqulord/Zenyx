a1bc440e9f5ed7cb74619122d7433e9a01b68f7e
faqulord <stylefaqu@gmail.com>
2026-01-28 15:01:52 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,34 +2,46 @@ import React from 'react';
 import './HeroSection.css';
 
 function HeroSection() {
-  // Képek a mozgó galériához (Attila termékei)
-  const galleryImages = [
-    "https://atharmonies.com/cdn/shop/files/20260114_123047.jpg?v=1768392509&width=1600",
-    "https://atharmonies.com/cdn/shop/files/20260113_172032.jpg?v=1768387309&width=1600",
-    "https://atharmonies.com/cdn/shop/files/20251007_154705.jpg?v=1761291463&width=1600",
-    "https://atharmonies.com/cdn/shop/files/20251028_104826.jpg?v=1762331002&width=1600",
-    "https://atharmonies.com/cdn/shop/files/20251007_154705.jpg?v=1761291463&width=1600"
+  
+  // ITT VANNAK A MÉDIA ELEMEK (Videó + Képek)
+  const mediaItems = [
+    {
+        type: 'video',
+        src: 'https://assets.mixkit.co/videos/preview/mixkit-craftsman-working-with-copper-wire-in-workshop-42686-large.mp4', // Ideiglenes műhely videó
+        poster: 'https://atharmonies.com/cdn/shop/files/20260114_123047.jpg?v=1768392509&width=1600'
+    },
+    { type: 'image', src: "https://atharmonies.com/cdn/shop/files/20260114_123047.jpg?v=1768392509&width=1600" },
+    { type: 'image', src: "https://atharmonies.com/cdn/shop/files/20260113_172032.jpg?v=1768387309&width=1600" },
+    { type: 'image', src: "https://atharmonies.com/cdn/shop/files/20251007_154705.jpg?v=1761291463&width=1600" },
+    { type: 'image', src: "https://atharmonies.com/cdn/shop/files/20251028_104826.jpg?v=1762331002&width=1600" }
   ];
 
   return (
     <div className='hero-wrapper'>
       
-      {/* 1. RÉSZ: A CÍM ÉS A MOZGÓ HÁTTÉR */}
       <div className='hero-header'>
         <h1 className='main-brand'>A&T HARMONIES</h1>
         <p className='brand-subtitle'>ONLINE MŰHELY</p>
       </div>
 
-      {/* MOZGÓ GALÉRIA SÁV */}
-      <div className='marquee-container'>
-        <div className='marquee-content'>
-            {/* Kétszer rendereljük a képeket, hogy végtelen legyen a mozgás */}
-            {galleryImages.map((img, i) => <img key={i} src={img} alt="Műhely munka" />)}
-            {galleryImages.map((img, i) => <img key={`dup-${i}`} src={img} alt="Műhely munka" />)}
-        </div>
+      {/* HÚZHATÓ GALÉRIA (SCROLL SNAP) */}
+      <div className='swipe-gallery'>
+        {mediaItems.map((item, index) => (
+            <div className='gallery-item' key={index}>
+                {item.type === 'video' ? (
+                    <video controls poster={item.poster} className='gallery-media'>
+                        <source src={item.src} type="video/mp4" />
+                        A böngésződ nem támogatja a videót.
+                    </video>
+                ) : (
+                    <img src={item.src} alt="Műhely pillanatkép" className='gallery-media' />
+                )}
+            </div>
+        ))}
       </div>
+      <p style={{textAlign:'center', fontSize:'0.8rem', color:'#999', marginTop:'5px'}}>⬌ Húzd el a galériát balra a többi képért!</p>
 
-      {/* 2. RÉSZ: KÜLDETÉSEM (A KÉPRŐL MÁSOLVA SZÓRÓL SZÓRA) */}
+      {/* KÜLDETÉS - ATTILA SZÖVEGE */}
       <div className='mission-section'>
         <h2 className='mission-title'>KÜLDETÉSEM</h2>
         <div className='mission-text'>
